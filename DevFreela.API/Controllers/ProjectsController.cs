ae17de2fbc8c37c91a3cc084e8dad776f623d169
Carlos Eduardo Lopes de Andrade <dev.andradecarlos@gmail.com>
2023-05-24 09:26:44 -0300

    Create base project

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,52 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProject)
+        {
+            if(createProject.Title.Length > 50)
+            {
+                return BadRequest();
+            }
+
+            return CreatedAtAction(nameof(GetById), new { Id = createProject.Id }, createProject);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProject)
+        {
+            if(updateProject.Description.Length > 200)
+            {
+                return BadRequest();
+            }
+
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var project = GetById(id);
+
+            if(project == null)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
+}
